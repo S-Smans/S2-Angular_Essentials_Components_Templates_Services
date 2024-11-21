fa36aa61900a32b75a8e9607de65da744f9058cd
Sandis <smanssandis@gmail.com>
2024-11-22 00:25:58 +0200

    all

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { TasksService } from './tasks.service';
+import { NewTaskData } from './task/task.model';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [TaskComponent, NewTaskComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
+})
+export class TasksComponent {
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
+  isAddingTask = false;
+
+  constructor(private tasksService: TasksService) {}
+
+  get selectedUserTasks() {
+    return this.tasksService.getUserTasks(this.userId);
+  }
+
+  onCompleteTask(id: string) {}
+
+  onAddTask(taskData: NewTaskData) {}
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+}
